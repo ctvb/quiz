ab7aad5bd5ae5c6a8b2c869f6f4b1b58c51c7ca3
ctvb <deenicks@hotmail.com>
2023-03-15 22:00:10 -0500

    added a new html and built some structure, added new js

--- script.js
+++ script.js
@@ -1,65 +1,3 @@
-// var timer = document.getElementById('countdown');
-// var main = document.getElementById('main');
-
-// var message = 'Questions';
-// var words = message.split(' ');
-
-// document.getElementById("question").innerHTML = "New text!";
-// document.getElementById("a1").innerHTML = "New text!";
-// document.getElementById("a2").innerHTML = "New text!";
-// document.getElementById("a3").innerHTML = "New text!";
-// document.getElementById("a4").innerHTML = "New text!";
-// // const startTime = Date.now();
-// // set duration
-// var duration = 60;
-// // get count down element from dom
-// var countdown = document.getElementById("countdown");
-
-// // update the countdown every second
-// var timer = setInterval(() => {
-//     var timeLeft = duration - Math.floor((Date.now() - startTime) / 1000);
-
-//     countdown.innerHTML = `Time Left: ${timeLeft} seconds`;
-
-//     if (timeLeft <= 0) {
-//         clearInterval(timer);
-//         countdown.innerHTML = "Time's up!";
-//     }
-// }, 1000);
-
-// set duration
-// var duration = 60;
-
-// // get countdown element from DOM
-// var countdown = document.getElementById("countdown");
-
-// // get start time
-// var startTime = Date.now();
-
-// // update the countdown every second
-// var timer = setInterval(() => {
-//     var timeLeft = duration - Math.floor((Date.now() - startTime) / 1000);
-
-// countdown.innerHTML = `Time Left: ${timeLeft} seconds`;
-
-//     if (timeLeft <= 0) {
-//         clearInterval(timer);
-//         countdown.innerHTML = "Time's up!";
-//     }
-// }, 1000);
-// var timerCount = 60
-// function startTimer () {
-//     // run countdown
-//     timerInterval = setInterval(function() {
-//         timerCount--;
-//         timerElement.textContent = timerCount;
-//         // if timer reaches 0, game lost
-//         if (timerCount === 0) {
-//             loseGame();
-//         }
-//     }, 1000);
-// }
-
 // Update question and answer options
 document.getElementById("question").innerHTML = "New question?";
 document.getElementById("a1").innerHTML = "Answer 1";
@@ -80,6 +18,7 @@ var startTime = Date.now();
 var timer = setInterval(() => {
     var timeLeft = duration - Math.floor((Date.now() - startTime) / 1000);
 
+    // countdown.innerHTML = `Time Left: ${timeLeft} seconds`;
     countdown.innerHTML = timeLeft + " seconds";
 
     if (timeLeft <= 0) {
