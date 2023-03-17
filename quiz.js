bff2f6cd2975773378420dbd54bc2f9d05160048
ctvb <deenicks@hotmail.com>
2023-03-17 00:59:01 -0500

    quiz.js

--- quiz.js
+++ quiz.js
@@ -12,63 +12,50 @@ var availableQuestions = [];
 
 var questions = [
   {
-    title: 'what is html used for?',
-    choices: ['The aesthetic of the page', 'The structure of the page', 'The interactive features of the page', 'all of the above'],
-    answer: 'all of the above'
+    question: 'What is HTML used for?',
+    choice1: 'The aesthetic of the page',
+    choice2: 'The structure of the page',
+    choice3: 'The interactive features of the page',
+    choice4: 'All of the above',
+    answer: 2,
   },
   {
-    title: 'what is html used for?',
-    choices: ['The aesthetic of the page', 'The structure of the page', 'The interactive features of the page', 'all of the above'],
-    answer: 'all of the above'
+    question: 'What does CSS stand for?',
+    choice1: 'Cascading Style Sheets',
+    choice2: 'Cascading Sheets Styled',
+    choice3: 'Cascaded Styled Sheets',
+    choice4: 'All of the above',
+    answer: 1,
   },
   {
-    title: 'what is html used for?',
-    choices: ['The aesthetic of the page', 'The structure of the page', 'The interactive features of the page', 'all of the above'],
-    answer: 'all of the above'
+    question: 'What aspect of web development is JavaScript responsible for?',
+    choice1: 'JavaScript makes everyaspect of a webpage',
+    choice2: 'JavaScript has nothing to do with web',
+    choice3: 'JavaScript deals with the interactive features of the page',
+    choice4: 'All of the above',
+    answer: 3,
   },
   {
-    title: 'what is html used for?',
-    choices: ['The aesthetic of the page', 'The structure of the page', 'The interactive features of the page', 'all of the above'],
-    answer: 'all of the above'
+    question: 'Where do you link a CSS file to HTML?',
+    choice1: 'The footer',
+    choice2: 'The header',
+    choice3: 'The divs',
+    choice4: 'The head',
+    answer: 4,
   },
-  // {
-  //   question: 'What does CSS stand for?',
-  //   choice1: 'Cascading Style Sheets',
-  //   choice2: 'Cascading Sheets Styled',
-  //   choice3: 'Cascaded Styled Sheets',
-  //   choice4: 'All of the above',
-  //   answer: 1,
-  // },
-  // {
-  //   question: 'What aspect of web development is JavaScript responsible for?',
-  //   choice1: 'JavaScript makes everyaspect of a webpage',
-  //   choice2: 'JavaScript has nothing to do with web',
-  //   choice3: 'JavaScript deals with the interactive features of the page',
-  //   choice4: 'All of the above',
-  //   answer: 3,
-  // },
-  // {
-  //   question: 'Where do you link a CSS file to HTML?',
-  //   choice1: 'The footer',
-  //   choice2: 'The header',
-  //   choice3: 'The divs',
-  //   choice4: 'The head',
-  //   answer: 4,
-  // },
-  // {
-  //   question: 'Where do you link a JS file?',
-  //   choice1: 'You cannot',
-  //   choice2: 'In the CSS file',
-  //   choice3: 'Towards the bottom of the HTML file',
-  //   choice4: 'All of the above',
-  //   answer: 3,
-  // },
+  {
+    question: 'Where do you link a JS file?',
+    choice1: 'You cannot',
+    choice2: 'In the CSS file',
+    choice3: 'Towards the bottom of the HTML file',
+    choice4: 'All of the above',
+    answer: 3,
+  },
 ];
 
-const SCORE_POINTS = 10;
+const SCORE_POINTS = 100;
 const MAX_QUESTIONS = 5;
 
-// Starts game
 startGame = () => {
   // availableQuestions = (...questions)
   getNewQuestion();
